6b0854b777a4d75a5c9009c546559e3bfb2dcffa
Travis Salad <travissalad@gmail.com>
2016-02-18 19:44:00 -0800

    added error messages for required fields, changed color of messages and customized messages for each input field.

--- js/main.js
+++ js/main.js
@@ -4,5 +4,81 @@
 // 1. Create a document ready handler.
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
-
+jQuery.validator.addMethod("lettersonly", function(value, element) {
+  return this.optional(element) || /^[a-z\s]+$/i.test(value);
+});
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).on('ready', function(){
+  $("#order-form").validate({
+    errorClass: "my-error-class",
+    validClass: "my-valid-class",
+    submitHandler: function(form){
+      form.submit();
+    },
+    rules: {
+      "your-name": {
+        required: true,
+        maxlength: 128,
+        lettersonly: true,
+        },
+      "your-address": {
+        required: true,
+      },
+      "your-city": {
+        required: true,
+      },
+      "your-state": {
+        required: true,
+        minlength: 2,
+        maxlength: 2
+      },
+      "your-zip": {
+        required: true,
+        minlength: 5,
+        maxlength: 5,
+        digits: true
+      },
+      "card-holder-name": {
+        required: true,
+        lettersonly: true,
+        maxlength: 128
+      },
+      "card-number": {
+        required: true,
+        creditcard: true,
+        minlength: 16,
+        maxlength: 19
+      },
+      "cvv": {
+        required: true,
+        digits: true,
+        minlength: 3,
+        maxlength: 3
+      },
+      "expiry-month": {
+        required: true,
+        min: 0
+      },
+      "shipping-method": {
+        required: true,
+      },
+      "comments": {
+        required: false,
+        maxlength: 500
+      },
+    },
+    messages: {
+      "your-name": "Please enter your first and last name.",
+      "your-address": "Please enter your street address.",
+      "your-city": "Please enter your city name.",
+      "your-state": "Please enter your 2 letter state abbreviation.",
+      "your-zip": "Please enter 5 digit zip code.",
+      "shipping-method": "please enter your preffered method for shipping.",
+      "expiry-month": "Please select a month.",
+      "shipping-method": "Please enter your preferred shipping method.",
+      "comments": "Must be less than 500 characters",
+    },
+
+  });
+  $('label span.glyphicon').tooltip();
+});
